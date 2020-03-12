469bbad96ec06df06887dbec15ec4aa38999d78b
Francis Rodson Clavel <frdclav@gmail.com>
2020-03-12 03:41:45 -0700

    adds code to get login api working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -15,12 +15,31 @@ module.exports = {
   //     .then(dbModel => res.json(dbModel))
   //     .catch(err => res.status(422).json(err));
   // },
-  // create: function(req, res) {
-  //   db.Book
-  //     .create(req.body)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
+  create: (req, res) => {
+    // console.log(req.body);
+    db.User
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  login: async (req, res) => {
+    try {
+      // console.log(req.body.username, req.body.password)
+
+      const user = await db.User.findOne({ username: req.body.username })
+      if (!user) {
+        return res.status(400).json({ message: "The username does not exist" });
+      }
+      user.comparePassword(req.body.password, (error, match) => {
+        if (!match) {
+          return res.status(400).json({ message: "The password is invalid" });
+        }
+      });
+      res.json({ message: "The username and password combination is correct!" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
   // update: function(req, res) {
   //   db.Book
   //     .findOneAndUpdate({ _id: req.params.id }, req.body)
