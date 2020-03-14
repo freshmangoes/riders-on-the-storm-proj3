55f58d64341e98114b16daa3616d48baeec3f8e4
Kyle Cilia <kylecilia@gmail.com>
2020-03-14 13:38:58 -0700

    Updates main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 // import MapboxMap from './Components/MapboxMap';
 import Mapv2 from './Components/Mapv2';
-import { UserLogin } from './Components/UserLogin';
 import React from 'react';
 
 import Navbar from './Components/Navbar';
@@ -10,10 +9,6 @@ function App() {
 		<div>
 			<Navbar />
 			<Input />
-
-			<p> Riders on the storm </p>
-			<UserLogin className='col-3' />
-			{/* <MapboxMap /> */}
 			<Mapv2 />
 		</div>
 	);
