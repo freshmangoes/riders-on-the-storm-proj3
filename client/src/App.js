af9f90f0e22a34bda09bbc2e86b50ff0be66d20d
Jonathan Truong <jjtruong90@gmail.com>
2020-03-14 09:47:23 -0700

    Updates Nav and Input

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { UserLogin } from "./Components/UserLogin"
-
+import Navbar from "./Components/Navbar"
+import Input from "./Components/Input"
 function App() {
   return (
     <div>
-
+      <Navbar />
+      <Input />
 
       < p > Riders on the storm </p >
       <UserLogin className='col-3' />
