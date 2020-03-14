9b4613744db174315146439000e43b324a5c2781
Kyle Cilia <kylecilia@gmail.com>
2020-03-14 13:13:38 -0700

    Adds directional comments to Mapv2.js

--- client/src/Components/Mapv2/Mapv2.js
+++ client/src/Components/Mapv2/Mapv2.js
@@ -2,14 +2,24 @@ import React, {Component} from 'react';
 import ReactMapGL, { GeolocateControl, NavigationControl } from 'react-map-gl';
 import Geocoder from 'react-map-gl-geocoder';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+
+/*
+TODO :: in no particular order, but definitely matters which one comes first
+	- Get ReactMapGL controls working
+	- Get drawing routes working
+		- GeoJSON, deck.gl, react-leaflet???
+	- Get custom data overlays working (for weather API)
+*/
 
 const TOKEN = 'pk.eyJ1IjoiZnJlc2hndWF2YXMiLCJhIjoiY2szM3k3Y2tmMHJmYTNjczJiNDVnZzhvOCJ9.Ry3fBcfenPpbHq86OrbN0Q';
 
 class Mapv2 extends Component {
 	state = {
 		viewport: {
-			width: '800px',
-			height: '600px',
+			// TODO :: Currently does not adjust viewport width and height with window. FIX THAT SOMEHOW ;)
+			width: '100vw',
+			height: '100vh',
 			latitude: 37.8715,
 			longitude: -122.273,
 			zoom: 12
@@ -21,7 +31,6 @@ class Mapv2 extends Component {
 	map = React.createRef();
 
 	handleViewportChange = viewport => {
-		// const oldviewport = this.state.viewport;
 		this.setState({
 			viewport: {...viewport}
 		});
@@ -34,15 +43,14 @@ class Mapv2 extends Component {
 				<ReactMapGL
 					ref={this.map}
 					{...this.state.viewport}
-					// width='100vw-80px'
-					// height='100vh-80px'
 					onViewportChange={this.handleViewportChange}
 					mapStyle='mapbox://styles/mapbox/streets-v11'
 					mapboxApiAccessToken = {TOKEN}
 				>
 					<Geocoder mapRef={this.map} mapboxApiAccessToken={TOKEN}/>
-					<GeolocateControl />
-					<NavigationControl />
+					{/* FIXME :: Things get ugly when these two lines are uncommented. Want to fix how this looks. */}
+					{/* <GeolocateControl /> */}
+					{/* <NavigationControl /> */}
 				</ReactMapGL>
 			</div>
 		)
