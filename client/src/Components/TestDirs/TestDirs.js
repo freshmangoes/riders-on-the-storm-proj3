e948dfaaf4a4f24c0652cb56afbe850b28089189
Kyle C <kylecilia@gmail.com>
2020-03-16 20:58:05 -0700

    Adds experimental component for getting directions - TestDirs

---
+++ client/src/Components/TestDirs/TestDirs.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+const axios = require('axios');
+
+const TOKEN = process.env.REACT_APP_MAP_TOKEN;
+class TestDirs extends Component {
+	getDirections = async () => {
+		try {
+			const data = await axios.get(
+				`https://api.mapbox.com/directions/v5/mapbox/driving/-122.50613655471308%2C37.538928163554345%3B-122.43398868283435%2C37.4615039973351?alternatives=true&geometries=geojson&steps=true&access_token=${TOKEN}`
+			);
+			console.log('data', data);
+		} catch (error) {
+			console.log('error', error);
+		}
+	};
+
+	render() {
+		this.getDirections();
+		return <div></div>;
+	}
+}
+
+export default TestDirs;
