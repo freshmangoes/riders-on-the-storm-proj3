c4634779c7d9f3ff6218dfd021444262b6c8a266
Riley Brenner <rbrenner1124@gmail.com>
2020-03-16 20:10:44 -0700

    weather

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -25,4 +25,27 @@ export default {
     //   saveBook: function(bookData) {
     //     return axios.post("/api/books", bookData);
     //   }
+
+    const getComboData = async (search, radius) => {
+        // async function to get weather data
+        const getWeather = async (search) => {
+            // city search parameter
+            const city = search;
+            // api key from dotenv
+            const apiKey = process.env.openWeather;
+            // get url for weather
+            //  uses a search for a city
+            const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+            // initialize data to be returned at the end
+            let data;
+            // es8 form factor promise to get data from api
+            try {
+                data = await axios.get(weatherUrl);
+            } catch (err) {
+                console.error('err::', err);
+            }
+            // return relevant json from data
+            return data.data;
+        };
+        
 };
