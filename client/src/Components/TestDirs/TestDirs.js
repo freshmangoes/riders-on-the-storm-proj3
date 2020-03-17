5148e84c3bf417028fd8ed330313d825856feedb
Kyle C <kylecilia@gmail.com>
2020-03-16 23:08:57 -0700

    Adds .env to gitignore because a pull messed it up

--- client/src/Components/TestDirs/TestDirs.js
+++ client/src/Components/TestDirs/TestDirs.js
@@ -8,7 +8,10 @@ class TestDirs extends Component {
 			const data = await axios.get(
 				`https://api.mapbox.com/directions/v5/mapbox/driving/-122.50613655471308%2C37.538928163554345%3B-122.43398868283435%2C37.4615039973351?alternatives=true&geometries=geojson&steps=true&access_token=${TOKEN}`
 			);
-			console.log('data', data);
+      console.log('data', data);
+
+      // array of arrays containing coordinate pairs
+      console.log('routes', data.data.routes[0].geometry.coordinates);
 		} catch (error) {
 			console.log('error', error);
 		}
