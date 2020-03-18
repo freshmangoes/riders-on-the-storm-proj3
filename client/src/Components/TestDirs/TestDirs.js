0fefa33273ef073795c463115e78fddd54bbb996
Kyle Cilia <kylecilia@gmail.com>
2020-03-18 16:49:14 -0700

    Uses geocoding API to get route now. Need to attach search box inputs to component.

--- client/src/Components/TestDirs/TestDirs.js
+++ client/src/Components/TestDirs/TestDirs.js
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 
 const TOKEN = process.env.REACT_APP_MAP_TOKEN;
-class TestDirs extends Component {
-	getDirections = async () => {
+const TestDirs = () => {
+	// Uses Mapbox directions API to get route
+	const getRoute = async (start, end) => {
+		const [startLon, startLat] = start;
+		const [endLon, endLat] = end;
+
+		const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${startLon},${startLat};${endLon},${endLat}?alternatives=true&geometries=geojson&steps=true&access_token=${TOKEN}`
 		try {
-			const data = await axios.get(
-				`https://api.mapbox.com/directions/v5/mapbox/driving/-122.50613655471308%2C37.538928163554345%3B-122.43398868283435%2C37.4615039973351?alternatives=true&geometries=geojson&steps=true&access_token=${TOKEN}`
-			);
-			console.log('data', data);
+			const data = await axios.get(url);
+			// console.log('data getRoute::', data);
 
 			// array of arrays containing coordinate pairs
-			console.log('routes', data.data.routes[0].geometry.coordinates);
+			// console.log('ROUTE::', data.data.routes[0].geometry.coordinates);
+			return data.data.routes[0].geometry.coordinates;
 		} catch (error) {
 			console.log('error', error);
 		}
 	};
 
-	render() {
-		this.getDirections();
-		return <div></div>;
+	// Uses Mapbox geocoding API to get coordinates
+	const getCoords = async (location) => {
+		const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?limit=1&access_token=${TOKEN}`;
+		try {
+			const data = await axios.get(url);
+			// console.log('data getCoords::', data)
+			// console.log('COORDS::', data.data.features[0].center);
+			return data.data.features[0].center;
+		} catch (err) {
+			console.log('err', err);
+		}
 	}
-}
+
+	//testing
+	const run = async () => {
+		const start = await getCoords('Folsom St, San Francisco');
+		const end = await getCoords('Ace Hardware, Half Moon Bay, CA');
+		const route = await getRoute(start, end);
+		console.log('start', start);
+		console.log('end', end);
+		console.log('route', route);
+	}
+
+	run();
+
+	return <div></div>;
+};
 
 export default TestDirs;
