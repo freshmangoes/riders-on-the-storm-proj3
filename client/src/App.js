7c1e4782ee4fc6046fe9617b4b2700c3c3ceb3b3
Kyle Cilia <kylecilia@gmail.com>
2020-03-18 17:11:55 -0700

    Removes TestDirs, makes Directions.js a util for accessing Mapbox APIs.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "./Components/Navbar"
 import {Input} from "./Components/Input"
 import Mapv2 from './Components/Mapv2'
-import TestDirs from './Components/TestDirs';
+
 function App() {
   return (
     <div>
@@ -11,7 +11,6 @@ function App() {
       <Input />
       < p > Riders on the storm </p >
       <Mapv2 />
-      <TestDirs/>
     </div>
 	);
 }
---
+++ client/src/utils/Directions.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const TOKEN = process.env.REACT_APP_MAP_TOKEN;
+
+export default {
+	getRoute: async (start, end) => {
+		const [startLon, startLat] = start;
+		const [endLon, endLat] = end;
+
+		// takes in (longitude, latitude) coordinate pairs and returns a list of coordinates for mapping
+		const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${startLon},${startLat};${endLon},${endLat}?alternatives=true&geometries=geojson&steps=true&access_token=${TOKEN}`;
+		try {
+			const data = await axios.get(url);
+			const route = data.data.routes[0].geometry.coordinates;
+			return route;
+		} catch (error) {
+			console.log('error', error);
+		}
+	},
+
+	// Uses Mapbox geocoding API to get coordinates
+	// takes in a location search and returns coordinates
+	getCoords: async location => {
+		const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?limit=1&access_token=${TOKEN}`;
+		try {
+			const data = await axios.get(url);
+			const coords = data.data.features[0].center;
+			// console.log('data getCoords::', data)
+			// console.log('COORDS::', data.data.features[0].center);
+			return data.data.features[0].center;
+		} catch (err) {
+			console.log('err', err);
+		}
+	}
+};
