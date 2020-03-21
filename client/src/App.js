298a7e76334ec2a622286d80880503d24d6b13d6
Kyle C <kylecilia@gmail.com>
2020-03-21 13:31:30 -0700

    Formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-
-import React from "react";
-import Navbar from "./Components/Navbar"
-import {Input, RouteContext} from "./Components/Input"
-import Mapv2 from './Components/Mapv2'
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { Input, RouteContext } from './Components/Input';
+import Mapv2 from './Components/Mapv2';
 
 function App() {
-  return (
-    <RouteContext.Provider value = {null}>
-
-      <div>
-        <Navbar />
-        <Input />
-
-        <Mapv2 />
-      </div>
-      
-    </RouteContext.Provider>
-  );
+	return (
+		<RouteContext.Provider value={null}>
+			<div>
+				<Navbar />
+				<Input />
+				<Mapv2 />
+			</div>
+		</RouteContext.Provider>
+	);
 }
 
 export default App;
