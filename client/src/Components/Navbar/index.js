0929b6423006bb422cbd4667787b485bbad0672b
Francis Rodson Clavel <frdclav@gmail.com>
2020-03-21 12:15:06 -0700

    'gets user login context working, need to create buttons for signout and saved searches'

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./style.css";
 import { UserLoginModal } from "../UserLoginModal/index"
+import { CurrentUserIdContext } from '../../Context/CurrentUserIdContext';
+import { UserLoggedInContext } from '../../Context/UserLoggedInContext';
 
 function Nav() {
+  const { currentUserId, setCurrentUserId } = useContext(CurrentUserIdContext);
+  const { userLoggedIn, setUserLoggedIn } = useContext(UserLoggedInContext);
+
   return (
     <nav className="navbar  navbar-dark  bg-dark">
       <a className="navbar-brand" href="/">
         Riders on the Storm
       </a>
-      <UserLoginModal buttonLabel='Sign In / Register' />
+
+      {
+        userLoggedIn ? `${currentUserId} is logged in` : <UserLoginModal buttonLabel='Sign In / Register' />
+      }
+
 
     </nav>
   );
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 
-
 export default {
     // Try to Login
     userLogin: (userData) => {
