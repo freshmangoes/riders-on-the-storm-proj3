0531495c209dc28debfd7f6c56d1fdbb54ff0233
Francis Rodson Clavel <frdclav@gmail.com>
2020-03-26 19:57:37 -0700

    change weather api call to https, remove comments in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,28 +18,13 @@ export default {
         return axios.get(`/api/search/findById/${id}`);
 
     },
-    //   // Gets all books
-    //   getBooks: function() {
-    //     return axios.get("/api/books");
-    //   },
-    //   // Gets the book with the given id
-    //   getBook: function(id) {
-    //     return axios.get("/api/books/" + id);
-    //   },
-    //   // Deletes the book with the given id
-    //   deleteBook: function(id) {
-    //     return axios.delete("/api/books/" + id);
-    //   },
-    //   // Saves a book to the database
-    //   saveBook: function(bookData) {
-    //     return axios.post("/api/books", bookData);
-    //   }
+
 
     getWeather: (search) => {
         // lat and lon search parameter
         const { lat, lon } = search;
         // get url for weather
-        const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
         // initialize data to be returned at the end
         return axios.get(weatherUrl)
     }
