c00dbfbd134707e9957665d914ea5d532d6d861f
Jonathan Truong <jjtruong90@gmail.com>
2020-03-27 16:02:47 -0700

    Adds some styling

--- client/src/Components/WeatherCard/index.js
+++ client/src/Components/WeatherCard/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Card, CardBody, CardHeader, CardText } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWind, faTemperatureLow } from '@fortawesome/free-solid-svg-icons';
+import './style.css'
 
 const WeatherCard = (props) => {
     // example:
@@ -45,11 +49,22 @@ const WeatherCard = (props) => {
     // This should show human readable data from the json above
     return (
         <div>
-            <h3>{weatherObj.name}</h3>
-            <p>Weather Overview: {weatherObj.weather[0].main}</p>
-            <p>Weather Description: {weatherObj.weather[0].description}</p>
-            <p> Temperature: {weatherObj.main.temp} F</p>
-            <p> Wind: {weatherObj.wind.speed} mph</p>
+            <Card>
+            <CardBody className="bg-secondary">
+            <CardHeader className="bg-dark text-warning">
+               <h3 id="city">{weatherObj.name}</h3>
+            </CardHeader>
+            <CardText className="text-white">
+            <p><strong>Weather Overview</strong>: {weatherObj.weather[0].main}</p>
+            <p><strong>Weather Description</strong>: {weatherObj.weather[0].description}</p>
+            <p><FontAwesomeIcon className="fa fa-lg" id="icon" icon={faTemperatureLow}></FontAwesomeIcon><strong>Temperature</strong>: {weatherObj.main.temp} F</p>
+            <p><FontAwesomeIcon className="fa fa-lg" id="icon" icon={faWind}></FontAwesomeIcon><strong>Wind</strong>: {weatherObj.wind.speed} mph</p>
+
+            </CardText>
+
+            </CardBody>
+
+            </Card>
 
         </div>
     )
