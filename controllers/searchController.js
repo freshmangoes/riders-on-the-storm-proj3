69f5cc406d76f70e3a825573c977d32ee4a683fb
Francis Rodson Clavel <frdclav@gmail.com>
2020-03-26 19:44:17 -0700

    removes comments from controllers, routes/api files

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,39 +1,21 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
-  // findAll: function(req, res) {
-  //   db.Book
-  //     .find(req.query)
-  //     .sort({ date: -1 })
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
   findById: function (req, res) {
     db.Search
       .find({ userId: req.params.id })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  // create: (req, res) => {
-  //   // console.log(req.body);
-  //   db.User
-  //     .create(req.body)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(00).json(err));
-  // },
   search: async (req, res) => {
     try {
-      // console.log(req.body, 'search body')
       const search = await db.Search.findOne({ start: req.body.start })
 
       if (!search) {
-        // console.log('adding', req.body)
         db.Search.create(req.body)
           .then(dbModel => res.json({ message: 'New search added!' }))
           .catch(error => res.status(200).json({ message: error }));
       } else {
-        // console.log('Invalid', req.body)
         res.status(200).json({ message: 'Search exists!' })
       }
 
@@ -41,21 +23,5 @@ module.exports = {
       console.log('err', error)
       res.status(500).json(error);
     }
-  },
-
-
-
-  // update: function(req, res) {
-  //   db.Book
-  //     .findOneAndUpdate({ _id: req.params.id }, req.body)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
-  // remove: function(req, res) {
-  //   db.Book
-  //     .findById({ _id: req.params.id })
-  //     .then(dbModel => dbModel.remove())
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // }
+  }
 };
